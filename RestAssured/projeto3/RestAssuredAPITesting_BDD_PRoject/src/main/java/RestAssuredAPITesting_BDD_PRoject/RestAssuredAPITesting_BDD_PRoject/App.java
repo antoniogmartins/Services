d36f5d4b459e4391c23a317730373da0f99ca612
antoniogmartins <antonio.martins@yahoo.com.br>
2026-06-14 07:55:21 -0300

    criando novo projeto

--- RestAssured/projeto3/RestAssuredAPITesting_BDD_PRoject/src/main/java/RestAssuredAPITesting_BDD_PRoject/RestAssuredAPITesting_BDD_PRoject/App.java
+++
@@ -1,7 +0,0 @@
-package RestAssuredAPITesting_BDD_PRoject.RestAssuredAPITesting_BDD_PRoject;
-
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
