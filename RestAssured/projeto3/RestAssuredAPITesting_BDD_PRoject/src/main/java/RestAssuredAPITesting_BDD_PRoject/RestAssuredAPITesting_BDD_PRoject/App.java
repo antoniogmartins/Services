aad956f27f26a927fa61d0e21076d837a96f1130
antoniogmartins <antonio.martins@yahoo.com.br>
2026-06-13 21:40:07 -0300

    criando novo projeto

---
+++ RestAssured/projeto3/RestAssuredAPITesting_BDD_PRoject/src/main/java/RestAssuredAPITesting_BDD_PRoject/RestAssuredAPITesting_BDD_PRoject/App.java
@@ -0,0 +1,7 @@
+package RestAssuredAPITesting_BDD_PRoject.RestAssuredAPITesting_BDD_PRoject;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ RestAssured/projeto3/RestAssuredAPITesting_BDD_PRoject/src/test/java/RestAssuredAPITesting_BDD_PRoject/RestAssuredAPITesting_BDD_PRoject/AppTest.java
@@ -0,0 +1,13 @@
+package RestAssuredAPITesting_BDD_PRoject.RestAssuredAPITesting_BDD_PRoject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(false);
+  }
+}
