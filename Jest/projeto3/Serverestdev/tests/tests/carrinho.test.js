41689355309ad4568a2961dc6506c042d0b9c83a
antoniogmartins <antonio.martins@yahoo.com.br>
2026-06-26 15:18:03 -0300

    atualizando os arquivos

--- Jest/projeto3/Serverestdev/tests/tests/carrinho.test.js
+++ Jest/projeto3/Serverestdev/tests/tests/carrinho.test.js
@@ -6,6 +6,7 @@ const url = 'https://serverest.dev';
 let token = "";
 let id_usuario = 0;
 let id_produto = 0;
+let id_produto_plus = 0;
 let _iemail = "";
 
 before(async function () {
@@ -35,42 +36,313 @@ describe('Carrinho - CAR', function () {
          console.log('O valor do token é: ' + token);
          });
 
-      it('USR-000 - Validar geração do ID do usuario', async function() { 
+      it('PRD-000 - Validar geração do ID do produto', async function() { 
         const response = await request(url)
-            .post('/usuarios')
+            .post('/produtos')
             .set("Content-Type", "application/json")
             .set("accept", "application/json")
             .set("authorization", token)
             .send({
-                     nome: 'Maria Delfina',
-                     email: `Maria${Date.now()}y@qa.com.br`,
-                     password: 'teste',
-                     administrador: 'false'
+                    nome: `Logitech MX Vertical_Modelo_X${Date.now()}Y`,
+                    preco: 470,
+                    descricao: 'Mouse',
+                    quantidade: 381
             })
-            id_usuario = response.body._id;
-            console.log('O valor do id é: ' + id_usuario);
-         expect(response.status).to.equal(201);
-         expect(response.body).to.have.property('_id');
+            id_produto = response.body._id;
+            console.log('O valor do id é: ' + id_produto);
+            expect(response.status).to.equal(201);
+            expect(response.body).to.have.property('_id');
+            expect(response.body.message).to.equal('Cadastro realizado com sucesso');
+
         });
       
-      it('CAR-001 - Adicionar produto ao carrinho', async function() { 
-        const response = await request(url)
-            .post('/usuarios')
+      it.skip('CAR-001 - Adicionar produto ao carrinho', async function() { 
+        
+        await request(url)
+            .del('/carrinhos/concluir-compra')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+        
+        const resposta = await request(url)
+            .post('/produtos')
             .set("Content-Type", "application/json")
             .set("accept", "application/json")
             .set("authorization", token)
             .send({
-                     nome: 'Maria Delfina',
-                     email: `Maria${Date.now()}y@qa.com.br`,
-                     password: 'teste',
-                     administrador: 'false'
-            })
-            id_usuario = response.body._id;
-            console.log('O valor do id é: ' + id_usuario);
+                    nome: `Logitech MX Vertical_Modelo_X${Date.now()}Y`,
+                    preco: 470,
+                    descricao: 'Mouse',
+                    quantidade: 381
+                 });
+            id_produto = resposta.body._id;
+
+        const response = await request(url)
+            .post('/carrinhos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                     produtos: [{
+                         idProduto: `${id_produto}`,
+                         quantidade: 1
+                 }]
+                });    
+
          expect(response.status).to.equal(201);
+         expect(response.body.message).to.equal('Cadastro realizado com sucesso');
          expect(response.body).to.have.property('_id');
         });
  
+      it.skip('CAR-002 - Adicionar multiplos produtos ao carrinho', async function() { 
+        
+        await request(url)
+            .del('/carrinhos/concluir-compra')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+        
+        const resposta = await request(url)
+            .post('/produtos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                    nome: `Logitech MX Vertical_Modelo_X${Date.now()}Y`,
+                    preco: 100,
+                    descricao: 'Mouse',
+                    quantidade: 301
+                 });
+            id_produto = resposta.body._id;
+
+        const outraresposta = await request(url)
+            .post('/produtos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                    nome: `Logitech MX Vertical_Modelo_X${Date.now()}Y`,
+                    preco: 200,
+                    descricao: 'Teclado',
+                    quantidade: 400
+                 });
+            id_produto_plus = outraresposta.body._id;
+
+        const response = await request(url)
+            .post('/carrinhos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                     produtos: [
+                {
+                         idProduto: `${id_produto}`,
+                         quantidade: 1
+                },
+                {
+                         idProduto: `${id_produto_plus}`,
+                         quantidade: 1
+                }
+                              ]
+                });    
+
+         expect(response.status).to.equal(201);
+         expect(response.body.message).to.equal('Cadastro realizado com sucesso');
+         expect(response.body).to.have.property('_id');
+        });
+
+      it.skip('CAR-003 - Adicionar produto com quantidade disponivel ao carrinho', async function() { 
+        
+        await request(url)
+            .del('/carrinhos/concluir-compra')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+        
+        const resposta = await request(url)
+            .post('/produtos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                    nome: `Logitech MX Vertical_Modelo_X${Date.now()}Y`,
+                    preco: 100,
+                    descricao: 'MousePad',
+                    quantidade: 2
+                 });
+            id_produto = resposta.body._id;
+
+       const response = await request(url)
+            .post('/carrinhos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                     produtos: [
+                {
+                         idProduto: `${id_produto}`,
+                         quantidade: 1
+                }
+                              ]
+                });    
+
+         expect(response.status).to.equal(201);
+         expect(response.body.message).to.equal('Cadastro realizado com sucesso');
+         expect(response.body).to.have.property('_id');
+        });
+
+      it.skip('CAR-004 - Validar cálculo do valor total do carrinho', async function() { 
+        
+        await request(url)
+            .del('/carrinhos/concluir-compra')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+        
+        const adicionarproduto1 = await request(url)
+            .post('/produtos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                    nome: `Logitech MX Vertical_Modelo_X${Date.now()}Y`,
+                    preco: 100,
+                    descricao: 'Mouse',
+                    quantidade: 301
+                 });
+            id_produto = adicionarproduto1.body._id;
+
+        const adicionarproduto2 = await request(url)
+            .post('/produtos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                    nome: `Logitech MX Vertical_Modelo_X${Date.now()}Y`,
+                    preco: 200,
+                    descricao: 'Teclado',
+                    quantidade: 400
+                 });
+            id_produto_plus = adicionarproduto2.body._id;
+
+        const adicionarprodutoaocarrinho = await request(url)
+            .post('/carrinhos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                     produtos: [
+                {
+                         idProduto: `${id_produto}`,
+                         quantidade: 1
+                },
+                {
+                         idProduto: `${id_produto_plus}`,
+                         quantidade: 1
+                }
+                              ]
+                });    
+
+         expect(adicionarprodutoaocarrinho.status).to.equal(201);
+         expect(adicionarprodutoaocarrinho.body.message).to.equal('Cadastro realizado com sucesso');
+         expect(adicionarprodutoaocarrinho.body).to.have.property('_id');
+
+        const consultarprodutonocarrinho = await request(url)
+            .get('/carrinhos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+
+         //console.log(consultarprodutonocarrinho.body);
+         expect(consultarprodutonocarrinho.status).to.equal(200);
+         
+           const carrinhos = consultarprodutonocarrinho.body.carrinhos[0];
+           const somaQuantidades = carrinhos.produtos.reduce(
+                (total, produto) => {return total + produto.quantidade;}, 0);
+
+         expect(carrinhos.quantidadeTotal).to.equal(somaQuantidades);
+
+         console.log(carrinhos.quantidadeTotal);
+         console.log(somaQuantidades);
+        
+        });
+
+        it.skip('CAR-005 - Validar cálculo do valor total do carrinho', async function() { 
+        
+        await request(url)
+            .del('/carrinhos/concluir-compra')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+        
+        const adicionarproduto1 = await request(url)
+            .post('/produtos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                    nome: `Logitech MX Vertical_Modelo_X${Date.now()}Y`,
+                    preco: 100,
+                    descricao: 'Mouse',
+                    quantidade: 301
+                 });
+            id_produto = adicionarproduto1.body._id;
+
+        const adicionarproduto2 = await request(url)
+            .post('/produtos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                    nome: `Logitech MX Vertical_Modelo_X${Date.now()}Y`,
+                    preco: 200,
+                    descricao: 'Teclado',
+                    quantidade: 400
+                 });
+            id_produto_plus = adicionarproduto2.body._id;
+
+        const adicionarprodutoaocarrinho = await request(url)
+            .post('/carrinhos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+            .send({
+                     produtos: [
+                {
+                         idProduto: `${id_produto}`,
+                         quantidade: 1
+                },
+                {
+                         idProduto: `${id_produto_plus}`,
+                         quantidade: 1
+                }
+                              ]
+                });    
+
+         expect(adicionarprodutoaocarrinho.status).to.equal(201);
+         expect(adicionarprodutoaocarrinho.body.message).to.equal('Cadastro realizado com sucesso');
+         expect(adicionarprodutoaocarrinho.body).to.have.property('_id');
+
+        const consultarprodutonocarrinho = await request(url)
+            .get('/carrinhos')
+            .set("Content-Type", "application/json")
+            .set("accept", "application/json")
+            .set("authorization", token)
+
+         //console.log(consultarprodutonocarrinho.body);
+         expect(consultarprodutonocarrinho.status).to.equal(200);
+         
+           const carrinhos = consultarprodutonocarrinho.body.carrinhos[0];
+           const somaPrecoTotal = carrinhos.produtos.reduce(
+                (total, produto) => {return total + (produto.quantidade * produto.precoUnitario);}, 0);
+
+         expect(carrinhos.precoTotal).to.equal(somaPrecoTotal);
+
+         //console.log(carrinhos.precoTotal);
+         //console.log(somaPrecoTotal);
+         
+        });
+
       });
     });
 });
